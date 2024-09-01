15e5c09d53c4490bc1436683ecff2b038bf8d135
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-09-01 15:16:37 +0800

    12. Mathf.Clamp() To Constrain Movement.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,17 +8,23 @@ public class PlayerControls : MonoBehaviour
     float xOffset;
     float yOffset;
     [SerializeField] float movementSpeed;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 5f;
+
     void Update()
     {
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
 
         xOffset = (xThrow * Time.deltaTime) * movementSpeed;
-        float newXPos = transform.localPosition.x + xOffset;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
         yOffset = (yThrow* Time.deltaTime) * movementSpeed;
-        float newYPos = transform.localPosition.y + yOffset;
-        transform.localPosition = new Vector3 (newXPos, newYPos, transform.localPosition.z);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3 (clampXPos, clampYPos, transform.localPosition.z);
         
           
     }
