1fc4580bea99a946b6cd5fd5787d80b3854dcf30
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-09-01 14:34:21 +0800

    9. Unity's New Input System
    Utilizing Unity's Input System

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+
     void Start()
     {
         
     }
 
+    private void OnEnable() 
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable() 
+    {
+        movement.Disable();
+    }
+
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        // float horizontalThrow = Input.GetAxis("Horizontal");
+        // float verticalThrow = Input.GetAxis("Vertical");
+
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
 
         Debug.Log(horizontalThrow);
         Debug.Log(verticalThrow);
