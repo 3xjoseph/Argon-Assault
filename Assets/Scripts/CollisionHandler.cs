0fee5959d91104c9164fe8f417824f4bca731ef1
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-09-04 18:05:14 +0800

    24. Reloading Scene After Collision
    - Understanding reloading scene by using OnTriggerCollision() then using scene manager to reload the scene whenever the ship collides with another game object.
    
    - Created function for Crash Sequence:
    -- Disables player movements
    -- Uses Invoke function to get a delay from the ReloadLevel() method

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-   void OnCollisionEnter(Collision other) 
-   {
-        Debug.Log($"{this.name} Collided with {other.gameObject.name}");
-   }
-
+     [Header("General Settings")]
+     [Tooltip("How many seconds of delay before the game reloads")]
+     [SerializeField] float loadLevelDelay;
    void OnTriggerEnter(Collider other) 
    {
-        Debug.Log($"{this.name} Collided with {other.gameObject.name}");
+          StartCrashSequence();
    }
 
+    void StartCrashSequence()
+    {
+          var playerControls = GetComponent<PlayerControls>();
+          playerControls.enabled = false;
+          Invoke("ReloadLevel", loadLevelDelay);
+    }
+
+    void ReloadLevel() 
+    {
+          int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+          SceneManager.LoadScene(currentSceneIndex);
+    }
 }
 
