d18889015e5d25d82f7df639054ad860c0452671
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-09-05 18:30:39 +0800

    28. Public Methods in Unity C#
    - Using public method to access it in other classes such as Enemy class and Scoreboard class.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.VFX;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform  parent;
-    private void OnParticleCollision(GameObject other) 
+
+    ScoreBoard scoreBoard;
+
+    [SerializeField] int scorePerHit = 5;
+
+    void Start() 
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessScore();
+        KillEnemy();
+    }
+
+    void KillEnemy()
     {
         GameObject explosionVFX = Instantiate(deathVFX, transform.position, Quaternion.identity);
         explosionVFX.transform.parent = parent;
         Destroy(gameObject);
+    }
+
+    void ProcessScore()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
     }
 }
