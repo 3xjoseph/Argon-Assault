ed084cefa9bfbc44395e8b6627bacc34ff8949bc
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-09-05 18:03:15 +0800

    27. Instantiate at Runtime
    Using Instantiate to create a clone of the explosion particle fx.
    
    Created an empty game object to use it as a folder for the instantiated objects.
    
    Used Transform.parent to put the instantiated objects to the empty game object.
    
    Created a script to self-destruct for an x amount of time.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform  parent;
     private void OnParticleCollision(GameObject other) 
     {
-        Debug.Log($"{this.name} Got hit by spaceship {other}");
+        GameObject explosionVFX = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        explosionVFX.transform.parent = parent;
         Destroy(gameObject);
     }
 }
